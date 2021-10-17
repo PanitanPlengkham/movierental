14ea9e3589e36cc845ba945b72250a2329241fb6
PanitanPlengkham <panitan.pl@ku.th>
2021-10-17 13:11:08 +0700

    Finish all task work

---
+++ point.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class PriceCode(Enum):
+    """An enumeration for different kinds of movies and their behavior"""
+    new_release = {"price": lambda days: 3.0 * days,
+                   "frp": lambda days: days}
+
+    normal = {"price": lambda days: 2 if days <= 2 else 2 + (1.5 * (days - 2)),
+              "frp": lambda days: 1}
+
+    childrens = {"price": lambda days: 1.5 if days <= 3 else 1.5 + (1.5 * (days - 3)),
+                 "frp": lambda days: 1}
+
+    def price(self, days: int) -> float:
+        "Return the rental price for a given number of days"""
+        pricing = self.value["price"]  # the enum member's price formula
+        return pricing(days)
+
+    def frp(self, days: int) -> float:
+        point = self.value["frp"]
+        return point(days)
